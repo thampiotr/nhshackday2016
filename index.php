20bab25fb9af6f9abe0f49203873c27e7c353b53
karnival <david@calpol.us>
2016-05-14 13:19:39 +0100

    Set up passing of search terms to search.php.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <h1>Hello NHS Hack Day</h1>
 
-<form action='search.php'>
+<form action='search.php' method='post'>
   Condition:<br>
   <input type="text" name="condition"><br>
   Intervention:<br>
