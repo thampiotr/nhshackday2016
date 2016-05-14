8c9d73434730e43080dbffac85f310e5ace60a57
karnival <david@calpol.us>
2016-05-14 13:23:13 +0100

    Fixed concatenation of strings for URL.

--- search.php
+++ search.php
@@ -6,36 +6,40 @@ Search terms:
         <?php
         if(isset($_POST['condition'])) {
                 echo $_POST['condition'];
+                $condition = $_POST['condition'];
         }
         ?>
 	<li>Intervention: </li>
         <?php
         if(isset($_POST['intervention'])) {
                 echo $_POST['intervention'];
+                $intervention = $_POST['intervention'];
         }
         ?>
 	<li>Outcome: </li>
         <?php
         if(isset($_POST['outcome'])) {
                 echo $_POST['outcome'];
+                $outcome = $_POST['outcome'];
         }
         ?>
 </ul>
 <h1>Contents of NCT00001372</h1>
 <pre>
 <?php
-	//$url = 'https://clinicaltrials.gov/ct2/results/download?down_stds=all&down_typ=results'+
-	//'&down_flds=shown&down_fmt=plain&term='
-	//+urlencode($condition) //Condition
-	//+'&rslt=With&intr='
-	//+urlencode($intervention) //Intervention
-	//+'&outc='
-	//+urlencode($outcome) //Outcome
-	//+'&show_down=Y';
+	$url = 'https://clinicaltrials.gov/ct2/results/download?down_stds=all&down_typ=results'.
+	'&down_flds=shown&down_fmt=plain&term='
+	.urlencode($condition) //Condition
+	.'&rslt=With&intr='
+	.urlencode($intervention) //Intervention
+	.'&outc='
+	.urlencode($outcome) //Outcome
+	.'&show_down=Y';
 
 	//$response = file_get_contents('https://clinicaltrials.gov/show/NCT00001372?displayxml=true');
 	//$response = new SimpleXMLElement($response);
 	//print_r($response->asXML())
+        echo "url is " . $url;
 ?>
 
 </pre>
