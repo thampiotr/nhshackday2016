12c6aa6f89187cddd640b9ea30aaee3b89fc1e7a
thampiotr <nospam-accepted-gwizdek@gmail.com>
2016-05-14 12:10:15 +0100

    Adding the search form stub

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <html>
 <h1>Hello NHS Hack Day</h1>
+
+<form>
+  Condition:<br>
+  <input type="text" name="condition"><br>
+  Intervention:<br>
+  <input type="text" name="intervention"><br>
+  Outcome:<br>
+  <input type="text" name="outcome">
+</form>
+
+<pre>
+<?php
+	// $response = file_get_contents('https://clinicaltrials.gov/show/NCT00001372?displayxml=true');
+	// print_r($response);
+?>
+</pre>
 </html>
