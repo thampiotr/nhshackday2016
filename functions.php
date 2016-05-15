3edb3d9be710f07d6922ebc9917c5ed9c6919f95
karnival <david@calpol.us>
2016-05-15 10:22:54 +0100

    Add some checking to handle studies without descriptions, etc.

--- functions.php
+++ functions.php
@@ -13,16 +13,16 @@ function trialParser ($xmlObject, $userDefinedOutcome)
 	//Cycle through the outcomes list, and try to identify the first 
 	foreach ($input['clinical_results']['outcome_list']['outcome'] as $key=>$value)
 	{
-			//So explode the subobject by the user defined outcome.
-			$tempArray = explode($userDefinedOutcome, strtolower($value['title']));
-			//rint_r($tempArray);
-			//Now we count the number of array elements. If >1, then the term is found
-			if (count($tempArray) >= 2)
-			{
-				//Set the $outcome variable [i.e. the one we are meeting]
-				$outcome = $value; 
-				break;
-			}
+		//So explode the subobject by the user defined outcome.
+		$tempArray = explode($userDefinedOutcome, strtolower($value['title']));
+		//rint_r($tempArray);
+		//Now we count the number of array elements. If >1, then the term is found
+		if (count($tempArray) >= 2)
+		{
+			//Set the $outcome variable [i.e. the one we are meeting]
+			$outcome = $value; 
+			break;
+		}
 	}	 
 
 	//$outcome contains the matched outcome
@@ -35,65 +35,72 @@ function trialParser ($xmlObject, $userDefinedOutcome)
         //In 1 direction: group ID, in the other: value
 
 	//First populate an array with groupdata
-	$groupData = array();
-	$counter = 0;
-	foreach ($outcome['group_list']['group'] as $key=>$value)
-	{	
-		$groupData[$counter]['id'] = $value['@attributes']['group_id'];
-		$groupData[$counter]['title'] = $value['title'];
-		$groupData[$counter]['description'] = $value['description'];
-		$counter++;
-	}
-
-        //Set a value for units
-        foreach ($outcome['measure_list']['measure'] as $key=>$value)
-        {
-        	$globalUnits=$value['units'];
+        if(isset($outcome)) {
+        	$groupData = array();
+        	$counter = 0;
+        
+        	foreach ($outcome['group_list']['group'] as $key=>$value)
+        	{	
+        		$groupData[$counter]['id'] = $value['@attributes']['group_id'];
+        		$groupData[$counter]['title'] = $value['title'];
+                        if(isset($value['description'])) {
+        		        $groupData[$counter]['description'] = $value['description'];
+                        }
+        		$counter++;
+        	}
+        
+                //Set a value for units
+                foreach ($outcome['measure_list']['measure'] as $key=>$value)
+                {
+                	$globalUnits=$value['units'];
+                }
+        
+                //Now populate it with the POPULATION SIZE data
+        	foreach ($outcome['measure_list']['measure'][0]['category_list']['category']['measurement_list']['measurement'] as $key=>$value)
+        	{
+        		//Lookup $value['@attributes']['group_id'] in [$groupData][$counter]['id'] and set $value['@attributes']['value']
+        		//Cycle through the group data
+        		foreach ($groupData as $key => $groupValue) {
+        
+        			if ($groupData[$key]['id'] == $value['@attributes']['group_id'])
+        			{
+        				$groupData[$key]['datasetSize'] = $value['@attributes']['value'];
+        				$groupData[$key]['units'] = $globalUnits;
+        			}
+        		}
+        	}
+        
+                //Now populate it with NUMBER MEETING CONDITION data
+                if(isset($outcome['measure_list']['measure'][1]['category_list']['category']['measurement_list']['measurement']) && isset($outcome['population'])) {
+        	        foreach ($outcome['measure_list']['measure'][1]['category_list']['category']['measurement_list']['measurement'] as $key=>$value)
+        	        {
+        	        	//Lookup $value['@attributes']['group_id'] in [$groupData][$counter]['id'] and set $value['@attributes']['value']
+        	        	//Cycle through the group data
+        	        	foreach ($groupData as $key => $groupValue) {
+        
+        	        		if ($groupData[$key]['id'] == $value['@attributes']['group_id'])
+        	        		{
+        	        			$groupData[$key]['numberMeetingCondition'] = $value['@attributes']['value'];
+        	        		}
+        	        	}
+        	        }
+        
+                        //Now we do the stastical analysis
+        
+        
+        
+        	        /*echo "<pre>";
+        	        print_r($groupData);
+        	        echo "</pre>";*/
+        
+        	        $outputArray = array();
+        	        $outputArray['trialID'] = $input['id_info']['nct_id'];
+        	        $outputArray['outcome']=array('type'=>$outcome['type'], 'title'=>$outcome['title'], 'description'=>$outcome['description'],'time_frame'=>$outcome['time_frame'],'population'=>$outcome['population']);
+        	        $outputArray['trialResults'] = $groupData;
+        	        
+        	        return $outputArray;
+                }
         }
-
-        //Now populate it with the POPULATION SIZE data
-	foreach ($outcome['measure_list']['measure'][0]['category_list']['category']['measurement_list']['measurement'] as $key=>$value)
-	{
-		//Lookup $value['@attributes']['group_id'] in [$groupData][$counter]['id'] and set $value['@attributes']['value']
-		//Cycle through the group data
-		foreach ($groupData as $key => $groupValue) {
-
-			if ($groupData[$key]['id'] == $value['@attributes']['group_id'])
-			{
-				$groupData[$key]['datasetSize'] = $value['@attributes']['value'];
-				$groupData[$key]['units'] = $globalUnits;
-			}
-		}
-	}
-
-        //Now populate it with NUMBER MEETING CONDITION data
-	foreach ($outcome['measure_list']['measure'][1]['category_list']['category']['measurement_list']['measurement'] as $key=>$value)
-	{
-		//Lookup $value['@attributes']['group_id'] in [$groupData][$counter]['id'] and set $value['@attributes']['value']
-		//Cycle through the group data
-		foreach ($groupData as $key => $groupValue) {
-
-			if ($groupData[$key]['id'] == $value['@attributes']['group_id'])
-			{
-				$groupData[$key]['numberMeetingCondition'] = $value['@attributes']['value'];
-			}
-		}
-	}
-
-        //Now we do the stastical analysis
-
-
-
-	/*echo "<pre>";
-	print_r($groupData);
-	echo "</pre>";*/
-
-	$outputArray = array();
-	$outputArray['trialID'] = $input['id_info']['nct_id'];
-	$outputArray['outcome']=array('type'=>$outcome['type'], 'title'=>$outcome['title'], 'description'=>$outcome['description'],'time_frame'=>$outcome['time_frame'],'population'=>$outcome['population']);
-	$outputArray['trialResults'] = $groupData;
-	
-	return $outputArray;
 
         //Next we need to run stats on this to work out relative risk
 	//To do this, we define the group with index $group
